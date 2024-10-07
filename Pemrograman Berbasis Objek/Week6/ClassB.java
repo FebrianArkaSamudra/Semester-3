df948d2c05943a674482904c00a8b7c16ff10172
FebrianArkaSamudra <144299755+FebrianArkaSamudra@users.noreply.github.com>
2024-10-07 17:38:34 +0700

    run

--- Pemrograman Berbasis Objek/Week6/ClassB.java
+++ Pemrograman Berbasis Objek/Week6/ClassB.java
@@ -1,6 +1,10 @@
 public class ClassB extends ClassA{
     public int z;
 
+public void setZ(int z){
+    this.z = z;
+}
+
     public void getNilaiZ(){
         System.out.println("Nilai z: " +z);
     }
